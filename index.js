ba7d23072847c5076dbdb1f3c3cc2fb444feb03e
Gabriel Lopez <lopezg3000@gmail.com>
2021-07-29 10:14:41 -0500

    javascript added

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-const card = document.getElementById("card");
+/*
 
-card.addEventListener("click", flipCard);
-function flipCard() {
-  card.classList.toggle("flipCard");
+Reusable flipCardEvent Documentation
+HTML
+1. Added click event to each card div to call the flipcard function
+2. Gave each card div a different id
+3. Passed id of each card as an argument when calling the function
+
+JAVASCRIPT
+4. Stored the element by id in a constant called card
+5. Retrieve classes for card using classList
+6. toggle between (add or remove) flipCard. i.e class="card" or class='card flipCard'
+
+*/
+
+function flipCard(id) {
+  let card = document.getElementById(id);
+
+  card.classList.toggle('flipCard');
 }
